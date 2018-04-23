80ca3a1b76f5fc4ad439954aa5ae83fca9e444a9
JIeIIIa <stealth@bigmir.net>
2018-04-24 02:22:34 +0300

    Third

--- src/com/mypackage/Bar.java
+++ src/com/mypackage/Bar.java
@@ -1,7 +1,10 @@
 package com.mypackage;
 
+import java.util.Scanner;
+
 public class Bar {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello world from Bar)))");
     }
 }
--- src/com/mypackage/Foo.java
+++ src/com/mypackage/Foo.java
@@ -3,5 +3,6 @@ package com.mypackage;
 public class Foo {
     public static void main(String[] args) {
         System.out.println("Hello from Foo)))");
+        System.out.println("Hm...");
     }
 }
