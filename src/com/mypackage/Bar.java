9114ec322054da7c359b997fab0ad864910c777b
JIeIIIa <stealth@bigmir.net>
2018-04-24 02:49:22 +0300

    last commit

--- src/com/mypackage/Bar.java
+++ src/com/mypackage/Bar.java
@@ -3,7 +3,7 @@ package com.mypackage;
 public class Bar {
     public static void main(String[] args) {
         int i = 1;
-        System.out.println("Hello from Bar!!!");
+        System.out.println("Hell this");
         System.out.println("Bye-bye)))))))))))))))");
     }
 }
--- src/com/mypackage/Foo.java
+++ src/com/mypackage/Foo.java
@@ -2,7 +2,7 @@ package com.mypackage;
 
 public class Foo {
     public static void main(String[] args) {
-        System.out.println("First First First");
-        System.out.println("Hello from Foo!!!  :)");
+        System.out.println("HateThis");
+        System.out.println("Hate:)");
     }
 }
