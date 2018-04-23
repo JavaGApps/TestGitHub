b3aa1dfeefd31f95c88aca33bcec4e4f02615e55
JIeIIIa <stealth@bigmir.net>
2018-04-24 02:39:23 +0300

    Another commit

--- src/com/mypackage/Bar.java
+++ src/com/mypackage/Bar.java
@@ -2,7 +2,8 @@ package com.mypackage;
 
 public class Bar {
     public static void main(String[] args) {
+        int i = 1;
         System.out.println("Hello from Bar!!!");
-        System.out.println("Bye-bye");
+        System.out.println("Bye-bye)))))))))))))))");
     }
 }
