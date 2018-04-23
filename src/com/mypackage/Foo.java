7694fbc0a0b3d5ab11171561f1634e635b9c9b18
JIeIIIa <stealth@bigmir.net>
2018-04-24 02:21:26 +0300

    Second

--- src/com/mypackage/Foo.java
+++ src/com/mypackage/Foo.java
@@ -2,6 +2,6 @@ package com.mypackage;
 
 public class Foo {
     public static void main(String[] args) {
-        System.out.println("Hello from Foo");
+        System.out.println("Hello from Foo)))");
     }
 }
