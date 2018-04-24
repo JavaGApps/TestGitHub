a1cfd1f6b6beb450150532a0dd6d6b3d2410ed11
JIeIIIa <stealth@bigmir.net>
2018-04-24 03:28:44 +0300

    latest commit

--- src/com/mypackage/Bar.java
+++ src/com/mypackage/Bar.java
@@ -3,7 +3,7 @@ package com.mypackage;
 public class Bar {
     public static void main(String[] args) {
         int i = 1;
-        System.out.println("Hell this");
+        System.out.println("NOT_Hell this");
         System.out.println("Bye-bye)))))))))))))))");
     }
 }
