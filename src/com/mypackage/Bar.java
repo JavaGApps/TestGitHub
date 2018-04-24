2a308376062c19720d60e116fb400e8426601915
JIeIIIa <stealth@bigmir.net>
2018-04-24 05:52:44 +0300

    e-mail send test

--- src/com/mypackage/Bar.java
+++ src/com/mypackage/Bar.java
@@ -2,7 +2,7 @@ package com.mypackage;
 
 public class Bar {
     public static void main(String[] args) {
-        System.out.println("Send emall!!!");
+        System.out.println("Send e-mall!!!");
         System.out.println("Bye-bye-bye");
     }
 }
