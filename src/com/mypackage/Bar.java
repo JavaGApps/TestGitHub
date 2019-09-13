0e16845ad0865d6b5265b6a597421d0aa3e1d77f
Oleksii Onishchenko <o.onishchenko@finik.pro>
2019-09-13 20:01:21 +0300

    Add logic

--- src/com/mypackage/Bar.java
+++ src/com/mypackage/Bar.java
@@ -3,6 +3,7 @@ package com.mypackage;
 public class Bar {
     public static void main(String[] args) {
         System.out.println("Send e-mall!!!");
+        System.out.println("Proceed...");
         System.out.println("Bye-bye-bye");
     }
 }
